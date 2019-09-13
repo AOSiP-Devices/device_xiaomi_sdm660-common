9ce3f4e03c482d85ff74d960892149aac37c9cd1
Akhil Narang <akhilnarang.1999@gmail.com>
2019-09-13 18:41:30 +0530

    sdm660-common: check light from https://github.com/AOSIP-Devices/device_xiaomi_whyred/commits/pie/light
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- light/Light.cpp
+++ light/Light.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,30 +14,27 @@
  * limitations under the License.
  */
 
-//Author := dev_harsh1998, Isaac Chen
-
-#define LOG_TAG "android.hardware.light@2.0-service.xiaomi_sdm660"
+#define LOG_TAG "LightService"
 
 #include <log/log.h>
-#include <fstream>
+
 #include "Light.h"
 
-namespace android {
-namespace hardware {
-namespace light {
-namespace V2_0 {
-namespace implementation {
+#include <fstream>
 
-#define LEDS                       "/sys/class/leds/"
-#define LCD_LED                    LEDS "lcd-backlight/"
-#define BRIGHTNESS                 "brightness"
-#define RED                        LEDS "red/"
-#define BLINK                      "blink"
-#define DUTY_PCTS                  "duty_pcts"
-#define PAUSE_HI                   "pause_hi"
-#define PAUSE_LO                   "pause_lo"
-#define RAMP_STEP_MS               "ramp_step_ms"
-#define START_IDX                  "start_idx"
+#define LEDS            "/sys/class/leds/"
+
+#define LCD_LED         LEDS "lcd-backlight/"
+#define WHITE_LED       LEDS "red/"
+
+#define BLINK           "blink"
+#define BRIGHTNESS      "brightness"
+#define MAX_BRIGHTNESS  "max_brightness"
+#define DUTY_PCTS       "duty_pcts"
+#define PAUSE_HI        "pause_hi"
+#define PAUSE_LO        "pause_lo"
+#define RAMP_STEP_MS    "ramp_step_ms"
+#define START_IDX       "start_idx"
 
 /*
  * 8 duty percent steps.
@@ -52,23 +49,78 @@ namespace implementation {
  */
 static int32_t BRIGHTNESS_RAMP[RAMP_STEPS] = {0, 12, 25, 37, 50, 72, 85, 100, 85, 72, 50, 37, 25, 12, 0};
 
+namespace {
 /*
  * Write value to path and close file.
  */
 static void set(std::string path, std::string value) {
     std::ofstream file(path);
-    /* Only write brightness value if stream is open, alive & well */
-    if (file.is_open()) {
-        file << value;
-    } else {
-        /* Fire a warning a bail out */
-        ALOGE("failed to write %s to %s", value.c_str(), path.c_str());
+
+    if (!file.is_open()) {
+        ALOGW("failed to write %s to %s", value.c_str(), path.c_str());
         return;
     }
+
+    file << value;
 }
 
 static void set(std::string path, int value) {
     set(path, std::to_string(value));
+}
+
+static int get(std::string path) {
+    std::ifstream file(path);
+    int value;
+
+    if (!file.is_open()) {
+        ALOGW("failed to read from %s", path.c_str());
+        return 0;
+    }
+
+    file >> value;
+    return value;
+}
+
+static int getMaxBrightness(std::string path) {
+    int value = get(path);
+    ALOGW("Got max brightness %d", value);
+    return value;
+}
+
+static uint32_t getBrightness(const LightState& state) {
+    uint32_t alpha, red, green, blue;
+
+    /*
+     * Extract brightness from AARRGGBB.
+     */
+    alpha = (state.color >> 24) & 0xFF;
+    red = (state.color >> 16) & 0xFF;
+    green = (state.color >> 8) & 0xFF;
+    blue = state.color & 0xFF;
+
+    /*
+     * Scale RGB brightness if Alpha brightness is not 0xFF.
+     */
+    if (alpha != 0xFF) {
+        red = red * alpha / 0xFF;
+        green = green * alpha / 0xFF;
+        blue = blue * alpha / 0xFF;
+    }
+
+    return (77 * red + 150 * green + 29 * blue) >> 8;
+}
+
+static inline uint32_t scaleBrightness(uint32_t brightness, uint32_t maxBrightness) {
+    return brightness * maxBrightness / 0xFF;
+}
+
+static inline uint32_t getScaledBrightness(const LightState& state, uint32_t maxBrightness) {
+    return scaleBrightness(getBrightness(state), maxBrightness);
+}
+
+static void handleBacklight(Type /* type */, const LightState& state) {
+    uint32_t brightness = getScaledBrightness(state, getMaxBrightness(LCD_LED MAX_BRIGHTNESS));
+    set(LCD_LED BRIGHTNESS, brightness);
 }
 
 /*
@@ -86,55 +138,38 @@ static std::string getScaledRamp(uint32_t brightness) {
     return ramp;
 }
 
-static void handleXiaomiBacklight(Type /*type*/, const LightState& state) {
-    uint32_t brightness = state.color & 0xFF;
-    brightness *= 16; // HACK
-    set(LCD_LED BRIGHTNESS, brightness);
-}
-
 static void setNotification(const LightState& state) {
-    uint32_t redBrightness, brightness;
+    uint32_t whiteBrightness = getScaledBrightness(state, getMaxBrightness(WHITE_LED MAX_BRIGHTNESS));
 
-    /*
-     * Extract brightness from RGB.
-     */
-    redBrightness   = (state.color >> 16) & 0xFF;
-    brightness      = (state.color >> 24) & 0xFF;
-
-    /*
-     * Scale RGB brightness if the Alpha brightness is not 0xFF.
-     */
-    if (brightness != 0xFF) {
-        redBrightness   = (redBrightness * brightness) / 0xFF;
-    }
-
-    /* Turn off the leds (initially) */
-    set(RED BRIGHTNESS, 0);
-    set(RED BLINK, 0);
+    /* Disable blinking */
+    set(WHITE_LED BLINK, 0);
 
     if (state.flashMode == Flash::TIMED) {
         /*
          * If the flashOnMs duration is not long enough to fit ramping up
          * and down at the default step duration, step duration is modified
          * to fit.
-        */
+         */
         int32_t stepDuration = RAMP_STEP_DURATION;
         int32_t pauseHi = state.flashOnMs - (stepDuration * RAMP_STEPS * 2);
         int32_t pauseLo = state.flashOffMs;
 
         if (pauseHi < 0) {
+            //stepDuration = state.flashOnMs / (RAMP_STEPS * 2);
             pauseHi = 0;
         }
 
-        /* Red(Actually White) */
-        set(RED BLINK, 1);
-        set(RED START_IDX, 0 * RAMP_STEPS);
-        set(RED DUTY_PCTS, getScaledRamp(redBrightness));
-        set(RED PAUSE_LO, pauseLo);
-        set(RED PAUSE_HI, pauseHi);
-        set(RED RAMP_STEP_MS, stepDuration);
+        /* White */
+        set(WHITE_LED START_IDX, 0 * RAMP_STEPS);
+        set(WHITE_LED DUTY_PCTS, getScaledRamp(whiteBrightness));
+        set(WHITE_LED PAUSE_LO, pauseLo);
+        set(WHITE_LED PAUSE_HI, pauseHi);
+        set(WHITE_LED RAMP_STEP_MS, stepDuration);
+
+        /* Enable blinking */
+        set(WHITE_LED BLINK, 1);
     } else {
-        set(RED BRIGHTNESS, redBrightness);
+        set(WHITE_LED BRIGHTNESS, whiteBrightness);
     }
 }
 
@@ -152,7 +187,7 @@ static std::vector<std::pair<Type, LightState>> notificationStates = {
     { Type::BATTERY, offState },
 };
 
-static void handleXiaomiNotification(Type type, const LightState& state) {
+static void handleNotification(Type type, const LightState& state) {
     for(auto it : notificationStates) {
         if (it.first == type) {
             it.second = state;
@@ -168,13 +203,19 @@ static void handleXiaomiNotification(Type type, const LightState& state) {
 }
 
 static std::map<Type, std::function<void(Type type, const LightState&)>> lights = {
-    {Type::BACKLIGHT, handleXiaomiBacklight},
-    {Type::NOTIFICATIONS, handleXiaomiNotification},
-    {Type::BATTERY, handleXiaomiNotification},
-    {Type::ATTENTION, handleXiaomiNotification},
+    { Type::ATTENTION, handleNotification },
+    { Type::NOTIFICATIONS, handleNotification },
+    { Type::BATTERY, handleNotification },
+    { Type::BACKLIGHT, handleBacklight },
 };
 
-Light::Light() {}
+} // anonymous namespace
+
+namespace android {
+namespace hardware {
+namespace light {
+namespace V2_0 {
+namespace implementation {
 
 Return<Status> Light::setLight(Type type, const LightState& state) {
     auto it = lights.find(type);
@@ -186,16 +227,19 @@ Return<Status> Light::setLight(Type type, const LightState& state) {
     /*
      * Lock global mutex until light state is updated.
      */
-
     std::lock_guard<std::mutex> lock(globalLock);
+
     it->second(type, state);
+
     return Status::SUCCESS;
 }
 
 Return<void> Light::getSupportedTypes(getSupportedTypes_cb _hidl_cb) {
     std::vector<Type> types;
 
-    for (auto const& light : lights) types.push_back(light.first);
+    for (auto const& light : lights) {
+        types.push_back(light.first);
+    }
 
     _hidl_cb(types);
 
