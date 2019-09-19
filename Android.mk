DD3Boh/android_device_xiaomi_sdm660-common

sdm660-common: add in whyred support
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout tulip wayne,$(TARGET_DEVICE)),)
+ifneq ($(filter jasmine_sprout tulip wayne whyred,$(TARGET_DEVICE)),)
 
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/firmware; \
     ln -sf /dev/block/bootdevice/by-name/msadp \

setup-makefiles.sh
@@ -39,7 +39,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "jasmine_sprout tulip wayne"
+write_headers "jasmine_sprout tulip wayne whyred"
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
 

AOSiP-Devices/device_xiaomi_sdm660-common

Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout twolip wayne,$(TARGET_DEVICE)),)
+ifneq ($(filter jasmine_sprout twolip wayne whyred,$(TARGET_DEVICE)),)
 
 $(shell mkdir -p $(TARGET_OUT_VENDOR)/firmware)
 

setup-makefiles.sh
@@ -39,7 +39,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
-write_headers "jasmine_sprout twolip wayne"
+write_headers "jasmine_sprout twolip wayne whyred"
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
 