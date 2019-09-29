b2952dedf6291cb168be642b494ca542a63bff35
Akhil Narang <akhilnarang.1999@gmail.com>
2019-09-29 19:35:32 +0530

    sdm660-common: inherit legacy-um sepolicy
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -258,7 +258,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 PROTOBUF_SUPPORTED := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 #BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
